b79dfd210d71058d6821785a5a2ebffe961b187b
EnriqueCastroM <kcmtha@gmail.com>
2023-03-20 18:50:49 -0600

    fix database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './database.js'
 import server from './server.js'
-const PORT = process.env.PORT || 3977;
 
-server.start({ PORT }, ({ port }) => {
-  console.log(`Server is running on PORT ${PORT}`)
+server.start({ port: 3000 }, ({ port }) => {
+  console.log(`Server is running on PORT ${port}`)
 })
