b761d0471990e7821ef58bc9108062aab3fbd9da
EnriqueCastroM <kcmtha@gmail.com>
2023-03-20 19:34:10 -0600

    fix: dotenv port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './database.js'
 import server from './server.js'
-
-server.start({ port: 3000 }, ({ port }) => {
-  console.log(`Server is running on PORT ${port}`)
+const PORT = process.env.PORT || 3000
+server.start(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`)
 })
